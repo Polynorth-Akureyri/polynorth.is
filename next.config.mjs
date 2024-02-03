5bb60df3f3f19d3b0a7ca48eaaa012c150b66651
Jóhann Guðmundsson <johann@kolibri.is>
2024-02-03 17:40:11 +0000

    testing new config... maybe?!

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  reactStrictMode: true,
+
+  // Add basePath
+  basePath: '/github-pages',
 }
 
 export default nextConfig
